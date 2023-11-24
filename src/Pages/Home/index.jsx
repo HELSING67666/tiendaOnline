ac07ed1849fc49fac81a8b6e06971c73d9c1cb1b
HELSING67666 <HELSING67666@gmail.com>
2023-11-24 10:17:49 -0500

    Creación componente card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,30 @@
+import React from "react"
 import { Card } from "../../Components/Card"
 
+
 function Home () {
+  const [items, setItems] = React.useState(null)
+
+  React.useEffect(()=>{
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  })
 
   return (
     <div>Home
-      <Card/>
+
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map((item)=>(
+            
+            <Card data={item}
+            key={item.id}/>
+          
+          ))
+        }
+        </div>
+      
     </div>
 
   )
